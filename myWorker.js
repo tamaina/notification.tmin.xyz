8ebfb085563f2aa51296803f4361cf2843a0a89b
tamaina <tamaina@hotmail.co.jp>
2017-06-12 20:40:09 +0900

    🎉

--- myWorker.js
+++ myWorker.js
@@ -3,7 +3,7 @@ var cachepages = [
     "/index",
     "/index.html"
 ];
-var version = '0.0.2';
+var version = '0.0.3';
 this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(version).then(function(cache) {
